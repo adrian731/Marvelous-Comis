71d2db30109576c866cfea4c3a20372533d8c348
root <root@debian>
2016-06-06 12:36:11 +0200

    primer post

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -19,7 +19,7 @@ def busqueda1():
 
 @post('/resultado1')
 def busqueda1():
-	destino=request.forms.get('name')
+	name=request.forms.get('name')
 	payload = {'apikey':'fcd7230a0d69643d5bd4110504babd72','hash':'fe261e6ba1072612497588694d4e2738','ts':'1'}
 	payload['name']=name
 	r=requests.get(url_base+'v1/public/characters',params=payload)
