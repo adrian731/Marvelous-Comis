7e08be54c4c64e90bae699d27655c70a69c607b5
root <root@debian>
2016-06-08 13:55:40 +0200

    primer post

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='MarvelousComics',
+setup(name='YourAppName',
       version='1.0',
       description='OpenShift App',
-      author='Adrian Domingez',
-      author_email='adrian731gg@gmail.com',
+      author='Your Name',
+      author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['bottle','urllib2','requests','HTMLParser',],
      )
